986504b94f56143ebfd91dbf0a1c9c2d0bedb7db
Արմեն Բադալյան <armenbadal@gmail.com>
2026-09-26 22:11:51 +0400

    Տեղափոխել նախագծերը առանձին repository-ներ

--- apps/student/src/audio.ts
+++
@@ -1,5 +0,0 @@
-import { invoke } from "@tauri-apps/api/core";
-
-export async function playBell(): Promise<void> {
-  await invoke("play_bell");
-}
--- apps/student/src/types.ts
+++
@@ -1,7 +0,0 @@
-export interface Lesson {
-  start: string;
-  end: string;
-  lesson: string;
-}
-
-export type Schedule = Record<string, Lesson[]>;
